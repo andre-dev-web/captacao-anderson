cefb12d4553d54a5fb1b8a80472c35fead07ebad
André Rodrigues <andererodrigues2003.05@gmail.com>
2023-08-14 21:03:06 -0300

    route

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -20,6 +20,8 @@ export default function Form() {
       .catch((error) => {
         console.log(error);
       });
+      const router = useRouter()
+      router.push('/confirm')
     }
   return (
     <section className="text-white body-font">
@@ -70,9 +72,7 @@ export default function Form() {
                 <input placeholder="Seu Whatsapp" name="Whatsapp" type="text" className="w-full bg-white bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:ring-orange-200 focus:bg-transparent focus:border-orange-400 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
               </div>
             </div>
-            <a href="/confirm">
-              <button type="submit" className="inline-flex text-gray bg-orange-400 border-0 my-2 py-2 px-6 focus:outline-none hover:bg-orange-500 rounded text-lg">Quero Saber a Verdade</button>
-            </a>
+            <button type="submit" className="inline-flex text-gray bg-orange-400 border-0 my-2 py-2 px-6 focus:outline-none hover:bg-orange-500 rounded text-lg">Quero Saber a Verdade</button>
           </form>
         </div>
       </div>
