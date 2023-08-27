bf34047b1718e1eb72e64276f6fb02dd9620dd03
André Rodrigues <andererodrigues2003.05@gmail.com>
2023-08-26 22:05:07 -0300

    all fix

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -81,7 +81,7 @@ export default function Form() {
                 <input required placeholder="Seu whatsapp" name="Whatsapp" type="text" className="w-full bg-white focus:bg-transparent rounded border border-gray-300 focus:ring-2 focus:ring-orange-400 focus:border-orange-400 text-base outline-none text-black focus:text-white py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
               </div>
             </div>
-            <button type="submit" disabled={isSubmitting} className="w-1/2 inline-flex justify-center text-gray bg-orange-400 border-0 my-2 py-2 focus:outline-none hover:bg-orange-500 rounded text-lg transition-color duration-300">{isSubmitting ? 'Submitting...' : 'Quero Saber a Verdade'}</button>
+            <button type="submit" disabled={isSubmitting} className="w-1/2 inline-flex justify-center text-gray bg-orange-400 border-0 my-2 py-2 focus:outline-none hover:bg-orange-500 rounded text-lg transition-color duration-300">{isSubmitting ? 'Enviando...' : 'Quero Saber a Verdade'}</button>
           </form>
         </div>
       </div>
