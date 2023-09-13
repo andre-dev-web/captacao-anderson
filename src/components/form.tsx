79d18d62051ff4ed1680e5971f53c6c5adcb340a
andre-dev-web <andrerodrigues2003.05@gmail.com>
2023-09-13 15:44:30 -0300

    correct grammar

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -52,13 +52,13 @@ export default function Form() {
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start items-center">
           <h1 className="text-lg md:text3xl sm:text-4xl mb-4 font-medium text-white text-center">
-            Os conteúdos das aulas ao vivo foram <span className='text-orange-400'>transformadores!</span> 
+            Os conteúdos das aulas ao vivo foram <span className='text-orange-400'>TRANSFORMADORAS!</span> 
           </h1>
           <h2 className="text-lg md:text3xl sm:text-4xl  mb-4 font-medium text-white text-center">
-            Além de todo conteúdo entregamos também alguns materiais com tarefas exclusivas   
+            Além de todo conteúdo entregamos também alguns materiais com tarefas exclusivas.   
           </h2>
           <h2 className="text-lg md:text3xl sm:text-4xl  mb-4 font-medium text-white text-center">
-            Se você quer ter acesso a tudo isso entre agora para o nosso grupo e fique por dentro de nossos proximos eventos e conteúdos!          
+            Se você quer ter acesso a tudo isso entre agora para o nosso grupo e fique por dentro de nossos próximos eventos e conteúdos!          
           </h2>
           <form id='formData' className="w-full flex justify-center flex-col" onSubmit={(e) => Submit(e)}>
             <div className="flex w-full justify-center ">
@@ -81,7 +81,7 @@ export default function Form() {
                 <input required placeholder="Seu whatsapp" name="Whatsapp" type="text" className="w-full bg-white focus:bg-transparent rounded border border-gray-300 focus:ring-2 focus:ring-orange-400 focus:border-orange-400 text-base outline-none text-black focus:text-white py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
               </div>
             </div>
-            <button type="submit" disabled={isSubmitting} className="w-1/2 inline-flex justify-center text-gray bg-orange-400 border-0 my-2 py-2 focus:outline-none hover:bg-orange-500 rounded text-lg transition-color duration-300">{isSubmitting ? 'Enviando...' : 'Quero Saber a Verdade'}</button>
+            <button type="submit" disabled={isSubmitting} className="w-1/2 inline-flex justify-center text-gray bg-orange-400 border-0 my-2 py-2 focus:outline-none hover:bg-orange-500 rounded text-lg transition-color duration-300">{isSubmitting ? 'Enviando...' : 'Quero Fazer Parte'}</button>
           </form>
         </div>
       </div>
