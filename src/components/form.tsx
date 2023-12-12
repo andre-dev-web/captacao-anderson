5fc0a79a221c2b8645c742481a97446b1482223a
andre-dev-web <andrerodrigues2003.05@gmail.com>
2023-12-12 11:16:31 -0300

    Text chenge

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -37,7 +37,7 @@ export default function Form() {
   return (
     <section className="text-white body-font">
       <div className='bg-black flex md:flex-row flex-col items-center'>
-        <h2 className="mx-auto py-1 text-xl md:text3xl sm:text-4xl font-medium text-white">Inscrições encerradas!</h2>
+        <h2 className="mx-auto py-1 text-xl md:text3xl sm:text-4xl font-medium text-white">Desafio além das Milhas!</h2>
       </div>
       
       <div className="container mx-auto flex px-5 py-16 md:flex-row flex-col items-center">
@@ -51,14 +51,14 @@ export default function Form() {
           />
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start items-center">
-          <h1 className="text-lg md:text3xl sm:text-4xl mb-4 font-medium text-white text-center">
-            Os conteúdos do curso foram <span className='text-orange-400'>TRANSFORMADORES!</span> 
+          <h1 className="text-3xl mb-4 font-medium text-white text-center">
+            Nos dias 09/01 10/01 e 11/01 as 20h você tem um encontro marcado comigo.
           </h1>
-          <h2 className="text-lg md:text3xl sm:text-4xl  mb-4 font-medium text-white text-center">
-            Entregamos vários materiais além da oferta arrasadora.   
+          <h2 className="text-3xl mb-4 font-medium text-white text-center">
+            Serão 3 aulas gratuitas onde eu mesmo vou traçar junto com você um <span className="text-orange-400">caminho prático</span> para a realização do seu sonho e tenho certeza que <span className="text-orange-400">2024 vai ser o inicio de uma nova vida</span> para você e sua familia!
           </h2>
-          <h2 className="text-lg md:text3xl sm:text-4xl  mb-4 font-medium text-white text-center">
-            Cadastre-se para ter acesso as nossas próximas ofertas e bônus exclusivos. Entre para o nosso grupo seleto!          
+          <h2 className="text-3xl  mb-4 font-medium text-white text-center">
+            Topa entrar nessa jornada comigo? Preencha os dados abaixo e garanta agora sua vaga em nosso grupo seleto!          
           </h2>
           <form id='formData' className="w-full flex justify-center flex-col" onSubmit={(e) => Submit(e)}>
             <div className="flex w-full justify-center ">
@@ -81,7 +81,7 @@ export default function Form() {
                 <input required placeholder="Seu whatsapp" name="Whatsapp" type="text" className="w-full bg-white focus:bg-transparent rounded border border-gray-300 focus:ring-2 focus:ring-orange-400 focus:border-orange-400 text-base outline-none text-black focus:text-white py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
               </div>
             </div>
-            <button type="submit" disabled={isSubmitting} className="w-1/2 inline-flex justify-center text-gray bg-orange-400 border-0 my-2 py-2 focus:outline-none hover:bg-orange-500 rounded text-lg transition-color duration-300">{isSubmitting ? 'Enviando...' : 'Quero Fazer Parte'}</button>
+            <button type="submit" disabled={isSubmitting} className="w-1/2 inline-flex justify-center text-gray bg-orange-400 border-0 my-2 py-2 focus:outline-none hover:bg-orange-500 rounded transition-color duration-300">{isSubmitting ? 'Enviando...' : 'Quero Fazer Parte'}</button>
           </form>
         </div>
       </div>
