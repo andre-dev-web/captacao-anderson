9fa09235fa617a05eb684797eea6a5e7643b1b8b
andre-dev-web <andrerodrigues2003.05@gmail.com>
2024-01-07 17:22:37 -0300

    Chenge WhatsApp link

--- src/components/confirm.tsx
+++ src/components/confirm.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { useEffect } from 'react';
 
 export default function Confirm() {
-  const urlRedirecionamento = "https://chat.whatsapp.com/ClEN9Taks0E2hoEP6Uh6wF";
+  const urlRedirecionamento = "https://chat.whatsapp.com/CFzvQTMN1MgCXLommn6r5q";
   useEffect(() => {
     const delay = 5000;
 
