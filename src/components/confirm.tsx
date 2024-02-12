b551580f9bd743272b91149aa5f8b2774cd52d3f
andre-dev-web <andrerodrigues2003.05@gmail.com>
2024-02-12 17:19:48 -0300

    Please work :')

--- src/components/confirm.tsx
+++ src/components/confirm.tsx
@@ -1,5 +1,4 @@
 "use client"
-import Image from 'next/image'
 import { useEffect } from 'react';
 
 export default function Confirm() {
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,11 +1,8 @@
 "use client"
 import { useState } from 'react';
-export default function Form() {
-  const [procedimentoSelecionado, setProcedimentoSelecionado] = useState('');
-  const handleProcedimentoChange = (event: any) => {
-    setProcedimentoSelecionado(event.target.value);
-  };
+import Select from '@/components/select';
 
+export default function Form() {
   const [isSubmitting, setIsSubmitting] = useState(false);
   async function Submit(e:any) {
     e.preventDefault();
@@ -16,7 +13,7 @@ export default function Form() {
 
     try {
       const response = await fetch(
-        'https://script.google.com/macros/s/AKfycbwdiZ0ibj-2Z49xSruaRqbmB7d9UN_f0Y67DLFdwBcBck7-1WTUCUmExlkChdWW7s4W/exec',
+        'https://script.google.com/macros/s/AKfycbyCDnCh-q98YHwuN3qoLcDxrHgPCPN8xuCySsX0zaFZ--V49_SbjXlxIJ9Ee5EQHwJp/exec',
         {
           method: 'POST',
           body: formDatab,
@@ -67,21 +64,7 @@ export default function Form() {
             <div className="flex w-full justify-center ">
               <div className="mr-4 w-full">
                 <label className="leading-7 text-md text-white">Procedimento<span className="text-red-400">*</span></label><br/>
-                <select 
-                  required 
-                  name="Procedimento" 
-                  value={procedimentoSelecionado}
-                  onChange={handleProcedimentoChange} 
-                  className="w-full bg-white focus:bg-transparent rounded border border-gray-300 focus:ring-2 focus:ring-indigo-900 focus:border-indigo-900 text-black outline-none text-base focus:text-white py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                >
-                  <option value="" className="text-black">Clique para selecionar</option>
-                  <option value="Implante" className="text-black">Implante</option>
-                  <option value="Carga Imediata" className="text-black">Carga imediata</option>
-                  <option value="Faceta" className="text-black">Faceta</option>
-                  <option value="Bichectomia" className="text-black">Bichectomia</option>
-                  <option value="Clareamento" className="text-black">Clareamento</option>
-                  <option value="Prótese" className="text-black">Prótese</option>
-                </select>
+                <Select/>
               </div>
             </div>
             <button type="submit" disabled={isSubmitting} className="w-1/2 inline-flex justify-center text-gray bg-indigo-900 border-0 my-2 py-2 focus:outline-none hover:bg-indigo-950 rounded transition-color duration-300">{isSubmitting ? 'Enviando...' : 'Quero agendar a avaliação'}</button>
